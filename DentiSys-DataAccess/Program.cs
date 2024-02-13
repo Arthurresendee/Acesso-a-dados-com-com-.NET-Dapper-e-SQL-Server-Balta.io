670a5529d540c0d476339c8b4fbefe1fc508b2ab
Arthur Cs <Arthurresende2004@gmail.com>
2024-02-13 01:38:14 -0300

    Organizando

--- DentiSys-DataAccess/Program.cs
+++ DentiSys-DataAccess/Program.cs
@@ -5,16 +5,38 @@ using System.Net.NetworkInformation;
 
 var connectionString = "Server=localhost;Database=DB_DentiSys_DataAcces;User ID=sa;Password=root; TrustServerCertificate=true";
 
-var patient = new Patient()
-{
-    Id = Guid.NewGuid(),
-    Name = "Flavin do Pneu",
-    Age = 19,
-    Height = 1.85,
-    weight = 59
-};
 
-var insertPatients = @"insert into 
+
+using(var connection = new SqlConnection(connectionString))
+{
+    ListPatients(connection);
+
+    //CreatePatient(connection);
+
+    
+}
+
+static void ListPatients(SqlConnection connection)
+{
+    var patientsList = connection.Query<Patient>("SELECT * FROM PATIENTS");
+    foreach (var item in patientsList)
+    {
+        Console.WriteLine($"{item.Name} - {item.Age} - {item.Height} - {item.weight}");
+    }
+}
+
+static void CreatePatient(SqlConnection connection)
+{
+    var patient = new Patient()
+    {
+        Id = Guid.NewGuid(),
+        Name = "Flavin do Pneu",
+        Age = 19,
+        Height = 1.85,
+        weight = 59
+    };
+
+    var insertPatients = @"insert into 
         [Patients] 
     VALUES (
         @IdParam,
@@ -23,10 +45,8 @@ var insertPatients = @"insert into
         @HeightParam,
         @WeightParam)";
 
-using(var connection = new SqlConnection(connectionString))
-{
     // retorna a quanidade de linhas afetadas
-    var rows = connection.Execute(insertPatients, new 
+    var rows = connection.Execute(insertPatients, new
     {
         IdParam = patient.Id, //patient.Id/ Quando o parâmetro for o mesmo nome, pode somente colocar a propriedade no objeto anônimo.
         NameParam = patient.Name, //patient.Name
@@ -35,11 +55,4 @@ using(var connection = new SqlConnection(connectionString))
         WeightParam = patient.weight
     });
     Console.WriteLine($"{rows} linhas iseridas");
-
-    var patientsList = connection.Query<Patient>("SELECT * FROM PATIENTS");
-    foreach (var item in patientsList)
-    {
-        Console.WriteLine($"{item.Name} - {item.Age} - {item.Height} - {item.weight}");
-    }
-
 }

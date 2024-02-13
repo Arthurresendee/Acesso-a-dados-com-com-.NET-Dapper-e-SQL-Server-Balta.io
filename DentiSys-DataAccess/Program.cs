f6fc18e593cf17b287e2a9c56f72c1473296a35d
Arthur Cs <Arthurresende2004@gmail.com>
2024-02-13 01:17:24 -0300

    tratando a SQL Injection

--- DentiSys-DataAccess/Program.cs
+++ DentiSys-DataAccess/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Dapper;
 using DentiSys_DataAccess.Models;
 using Microsoft.Data.SqlClient;
+using System.Net.NetworkInformation;
 
 var connectionString = "Server=localhost;Database=DB_DentiSys_DataAcces;User ID=sa;Password=root; TrustServerCertificate=true";
 
@@ -13,10 +14,27 @@ var patient = new Patient()
     weight = 59
 };
 
-var insertPatients = "insert into [Patients] VALUES (Name, Age, Height, Weight)";
+var insertPatients = @"insert into 
+        [Patients] 
+    VALUES (
+        @IdParam,
+        @NameParam,
+        @AgeParam,
+        @HeightParam,
+        @WeightParam)";
 
 using(var connection = new SqlConnection(connectionString))
 {
+    // retorna a quanidade de linhas afetadas
+    var rows = connection.Execute(insertPatients, new 
+    {
+        IdParam = patient.Id, //patient.Id/ Quando o parâmetro for o mesmo nome, pode somente colocar a propriedade no objeto anônimo.
+        NameParam = patient.Name, //patient.Name
+        AgeParam = patient.Age,
+        HeightParam = patient.Height,
+        WeightParam = patient.weight
+    });
+    Console.WriteLine($"{rows} linhas iseridas");
 
     var patientsList = connection.Query<Patient>("SELECT * FROM PATIENTS");
     foreach (var item in patientsList)

2a38e3ab0741dbbf0051da8d7a88f99cb66e597d
Arthur Cs <Arthurresende2004@gmail.com>
2024-02-13 00:46:14 -0300

    Adicionar arquivos de projeto.

---
+++ DentiSys-DataAccess/Models/Patient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentiSys_DataAccess.Models
+{
+    public class Patient
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Age { get; set; }
+        public double Height { get; set; }
+        public double weight { get; set; }
+    }
+}
---
+++ DentiSys-DataAccess/Program.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using DentiSys_DataAccess.Models;
+using Microsoft.Data.SqlClient;
+
+var connectionString = "Server=localhost;Database=DB_DentiSys_DataAcces;User ID=sa;Password=root; TrustServerCertificate=true";
+
+var patient = new Patient()
+{
+    Id = Guid.NewGuid(),
+    Name = "Flavin do Pneu",
+    Age = 19,
+    Height = 1.85,
+    weight = 59
+};
+
+var insertPatients = "insert into [Patients] VALUES (Name, Age, Height, Weight)";
+
+using(var connection = new SqlConnection(connectionString))
+{
+
+    var patientsList = connection.Query<Patient>("SELECT * FROM PATIENTS");
+    foreach (var item in patientsList)
+    {
+        Console.WriteLine($"{item.Name} - {item.Age} - {item.Height} - {item.weight}");
+    }
+
+}
